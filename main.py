097661460d067f75032827e2164d255c641a159c
Criid <Lowry_46@hotmail.com>
2023-07-08 13:04:46 -0400

    changed several functions and added new ones.

--- main.py
+++ main.py
@@ -1,68 +1,93 @@
 import requests
+from time import sleep
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 TOKEN = os.environ.get("TOKEN")
+HEADERS = {"Content-Type": "application/json", "Authorization": f"Bearer {TOKEN}"}
 
-HEADERS = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {TOKEN}'
+
+def register():
+    header = {"Content-Type": "application/json"}
+    payload = {
+        "symbol": "RAVENOR",
+        "faction": "COSMIC",
     }
 
-payload = {
-    "symbol": "RAVENOR",
-    "faction": "COSMIC",
-   }
+    request = requests.post(
+        "https://api.spacetraders.io/v2/register", headers=header, json=payload
+    )
+    return request.json()
 
-# request = requests.post('https://api.spacetraders.io/v2/register', headers=headers, json=payload)
 
 def my_agent_info(header):
-    request = requests.get('https://api.spacetraders.io/v2/my/agent', headers=header)
+    request = requests.get("https://api.spacetraders.io/v2/my/agent", headers=header)
     return request.json()
+
 
 def view_contract(header):
-    request = requests.get('https://api.spacetraders.io/v2/my/contracts', headers=header)
-    return request.json()['data']
+    request = requests.get(
+        "https://api.spacetraders.io/v2/my/contracts", headers=header
+    )
+    return request.json()["data"]
+
 
 def accept_contract(header, contract_id):
-    request = requests.post(f'https://api.spacetraders.io/v2/my/contracts/{contract_id}/accept', headers=header)
+    request = requests.post(
+        f"https://api.spacetraders.io/v2/my/contracts/{contract_id}/accept",
+        headers=header,
+    )
     return request.json()
 
+
 def get_ship_info(header, ship_number):
-    request = requests.get(f'https://api.spacetraders.io/v2/my/ships/', headers=header)
-    return request.json()['data'][ship_number]
+    request = requests.get(f"https://api.spacetraders.io/v2/my/ships/", headers=header)
+    return request.json()["data"][ship_number]
+
+
+def all_ship_info(header):
+    request = requests.get(f"https://api.spacetraders.io/v2/my/ships/", headers=header)
+    return request.json()["data"]
 
 
 def get_systems(header):
-    request = requests.get('https://api.spacetraders.io/v2/systems', headers=header)
+    request = requests.get("https://api.spacetraders.io/v2/systems", headers=header)
     return request.json()
+
 
 def check_system(header):
     system = my_agent_info(header)
-    system_symbol = system['data']['headquarters'].split('-')
-    request = requests.get(f'https://api.spacetraders.io/v2/systems/{system_symbol[0]}-{system_symbol[1]}/waypoints/', headers=header)
-    return request.json()
+    system_symbol = system["data"]["headquarters"].split("-")
+    request = requests.get(
+        f"https://api.spacetraders.io/v2/systems/{system_symbol[0]}-{system_symbol[1]}/waypoints/",
+        headers=header,
+    )
+    return request.json()["data"]
+
 
 def check_waypoints(header):
-    request = requests.get(f'https://api.spacetraders.io/v2/systems/X1-YU85/waypoints')
+    request = requests.get(f"https://api.spacetraders.io/v2/systems/X1-YU85/waypoints")
     return request.json()
 
 
+def find_shipyard(header):
+    system = check_system(header)
 
-traits = check_waypoints(HEADERS)['data']
-
-# for item in traits:
-#     for trait in item['traits']:
-#         if trait['symbol'] == "SHIPYARD":
-#             print(item['symbol'])
+    for item in system:
+        for trait in item["traits"]:
+            if trait["symbol"] == "SHIPYARD":
+                return item["symbol"]
 
 
-
-def get_location(header):
-    request = requests.get('https://api.spacetraders.io/v2/systems/X1-YU85/waypoints/X1-YU85-34607X/shipyard', headers=header)
-    return request.json()
+def view_ships(header):
+    waypoint = find_shipyard(header)
+    system_symbol = waypoint.split("-")
+    request = requests.get(
+        f"https://api.spacetraders.io/v2/systems/{system_symbol[0]}-{system_symbol[1]}/waypoints/{waypoint}/shipyard"
+    )
+    return request.json()["data"]
 
 
 # shipyard = get_location(HEADERS)
@@ -85,38 +110,56 @@ def get_location(header):
 #         print(item['symbol'])
 
 
-def dock_or_orbit(header, command):
-    if command == "orbit":
-        request = requests.post('https://api.spacetraders.io/v2/my/ships/RAVENOR-4/orbit', headers=header)
-    if command == "dock":
-        request = requests.post('https://api.spacetraders.io/v2/my/ships/RAVENOR-4/dock', headers=header)
+def dock_or_orbit(header, ship_number):
+    ship = get_ship_info(header, ship_number)
+    ship_status = ship["nav"]["status"]
+    ship_symbol = ship["symbol"]
+    if ship_status == "DOCKED":
+        request = requests.post(
+            f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/orbit",
+            headers=header,
+        )
+    if ship_status == "IN_ORBIT":
+        request = requests.post(
+            f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/dock",
+            headers=header,
+        )
 
-    return request.json()['data']['nav']['status']
-
-
+    return request.json()["data"]["nav"]["status"]
 
 
 def navigate_ship(header):
     ship = "RAVENOR-4"
     data = {"waypointSymbol": "X1-YU85-76885D"}
-    request = requests.post(F'https://api.spacetraders.io/v2/my/ships/{ship}/navigate', headers=header, json=data)
-    return request.json()['data']['nav']['status']
+    request = requests.post(
+        f"https://api.spacetraders.io/v2/my/ships/{ship}/navigate",
+        headers=header,
+        json=data,
+    )
+    return request.json()["data"]["nav"]["status"]
 
 
 # for item in get_ship_info(HEADERS)['data'][3]:
 #     print(item)
 
-def extract_ore(header):
-    ship = "RAVENOR-4"
-    request = requests.post(f'https://api.spacetraders.io/v2/my/ships/{ship}/extract', headers=header)
+
+def extract_ore(header, ship_number):
+    ship = get_ship_info(header, ship_number)
+    ship_symbol = ship["symbol"]
+    request = requests.post(
+        f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/extract", headers=header
+    )
+
     try:
-        info = request.json()['data']
-        print(f"Yield: {info['extraction']['yield']['symbol']} - {info['extraction']['yield']['units']} Units")
+        info = request.json()["data"]
+        print(
+            f"Yield: {info['extraction']['yield']['symbol']} - {info['extraction']['yield']['units']} Units"
+        )
         print(f'Cooldown Time: {info["cooldown"]["totalSeconds"]}')
-        get_ship_cargo(header, 3)
+        return info["cooldown"]["totalSeconds"]
     except:
-        return request.json()['error']
-                              
+        return request.json()["error"]
+
 
 # for ship in get_ship_info(HEADERS):
 #     print(ship['registration'], "\n", ship['nav'], "\n")
@@ -126,41 +169,49 @@ def get_market_data(header):
     system_symbol = "X1-YU85"
     waypoint = "X1-YU85-34607X"
     request = requests.get(
-        f'https://api.spacetraders.io/v2/systems/{system_symbol}/waypoints/{waypoint}/market')
-    return request.json()['data']
+        f"https://api.spacetraders.io/v2/systems/{system_symbol}/waypoints/{waypoint}/market"
+    )
+    return request.json()["data"]
 
 
 def get_ship_cargo(header, ship_number):
-    ship_symbol = get_ship_info(header, ship_number)['symbol']
-    request = requests.get(f'https://api.spacetraders.io/v2/my/ships/{ship_symbol}/cargo', headers=header)
-    ship = request.json()['data']
+    ship_symbol = get_ship_info(header, ship_number)["symbol"]
+    request = requests.get(
+        f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/cargo", headers=header
+    )
+    ship = request.json()["data"]
     for item in ship:
         if item == "inventory":
             print("\n")
-            for cargo in ship['inventory']:
-                print(f"{cargo['name']}: {cargo['units']}" )
+            for cargo in ship["inventory"]:
+                print(f"{cargo['name']}: {cargo['units']}")
         else:
             print(f"{item.title()}: {ship[item]}")
 
 
-# data = {
-#     "symbol": "QUARTZ_SAND",
-#     "units": "5"
-# }
-# ship = ship_symbol = get_ship_info(HEADERS, 3)['symbol']
-# request = requests.post(f'https://api.spacetraders.io/v2/my/ships/{ship}/sell', headers=HEADERS, json=data)
-# print(request.json())
-
-
 def sell_all_cargo(header, ship_number):
     ship = get_ship_info(header, ship_number)
-    ship_symbol = ship['symbol']
-    for cargo in ship['cargo']['inventory']:
+    ship_symbol = ship["symbol"]
+    for cargo in ship["cargo"]["inventory"]:
         data = {}
-        data['symbol'] = cargo['symbol']
-        data['units'] = cargo['units']
-        request = requests.post(f'https://api.spacetraders.io/v2/my/ships/{ship_symbol}/sell', headers=header, json=data)
+        data["symbol"] = cargo["symbol"]
+        data["units"] = cargo["units"]
+        request = requests.post(
+            f"https://api.spacetraders.io/v2/my/ships/{ship_symbol}/sell",
+            headers=header,
+            json=data,
+        )
         print(request.json())
 
 
-get_ship_cargo(HEADERS, 3)
+def extract_all_ore():
+    ship = get_ship_info(HEADERS, 3)["cargo"]
+    ship_capacity = ship["capacity"]
+    ship_units = ship["units"]
+    while ship_units != ship_capacity:
+        sleep(extract_ore(HEADERS, 3))
+
+
+def auto_mine(header, ship_number):
+    credits = my_agent_info(header)
+    print(credits)
